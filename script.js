3eacd0dd2671e6ac040bb0f110d43b286a3d94bc
Igor2975 <89157738+Igor2975@users.noreply.github.com>
2022-02-01 21:50:31 +0300

    correction error

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const goods = [
+const products = [
     { title: 'Notebook', price: 2000,img:src='images/Notebook.jpg' },
     { title: 'Mouse', price: 20,img:src='images/Mouse.jpg' },
     { title: 'Keyboard', price: 200,img:src='images/Keyboard.jpg' },
     { title: 'Gamepad', price: 50,img:src='images/Gamepad.jpg' },
   ];
   
-  const renderGoodsItem = ({title = '', price = 0, img}) => `
-    <div class="goods-item">
-    <img class="goods-item-img" width ='180' height ='160' src=${img} alt="#" />
-    <h3 class="goods-item-title">${title}</h3>
-    <p class="goods-item-price">${price}</p>
-    <button class="goods-item-button" type="button">Добавить</button>
+  const renderProduct = ({title = '', price = 0, img}) => `
+    <div class="products-item">
+    <img class="products-item-img" width ='180' height ='160' src=${img} alt="#" />
+    <h3 class="products-item-title">${title}</h3>
+    <p class="products-item-price">${price}</p>
+    <button class="products-item-button" type="button">Добавить</button>
     </div>
   `
   
-  const renderGoodsList = () => {
-    let goodsList = goods.map(item => renderGoodsItem(item));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
+  const renderPage = () => {
+    let productList = products.map(item => renderProduct(item));
+    document.querySelector('.products-list').innerHTML = productList.join('');
   }
-  renderGoodsList();
+  renderPage();
